5a2dd9da6e0b6d9f83fe8b401f1173cc009a98bd
hyun6ik <fkanwm@naver.com>
2022-01-29 17:35:22 +0900

    Add #2 Jenkins 인프라 구성

---
+++ ec2-instance/Jenkins/templates/userdata.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sudo yum update
+sudo yum install -y docker git
+
+# docker
+sudo usermod -aG docker ec2-user
+systemctl enable docker
+systemctl start docker
+
+# docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# source
+cd /home/ec2-user
+git clone https://github.com/hyun6ik/Jenkins-Study.git
+chown -R ec2-user:ec2-user Jenkins-Study
+cd /home/ec2-user/Jenkins-Study/ec2-instance/jenkins/docker
+make run
