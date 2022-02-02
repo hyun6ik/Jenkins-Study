4a96c9e0e3348005898bfe4dcc0c3e2d9ea81e2f
hyun6ik <fkanwm@naver.com>
2022-02-02 21:36:19 +0900

    Add #7 : S3 Remote State 변경

--- ec2-instance/Jenkins/backend.tf
+++ ec2-instance/Jenkins/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "hyun6ik-backend"
-    key = "bastion/terraform.tfstate"
+    key = "jenkins/terraform.tfstate"
     region = "ap-northeast-2"
     max_retries = 3
   }
