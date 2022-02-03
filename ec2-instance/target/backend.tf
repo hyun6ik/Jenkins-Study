22d1547000357d6c8cdcec77c3b28e9e74a74a72
hyun6ik <fkanwm@naver.com>
2022-02-03 16:47:20 +0900

    Add #9 : target 서버 추가

--- ec2-instance/target/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket = "hyun6ik-backend"
-    key = "jenkins/terraform.tfstate"
-    region = "ap-northeast-2"
-    max_retries = 3
-  }
-}
--- ec2-instance/target/output.tf
+++
@@ -1,7 +0,0 @@
-output "ip" {
-  value = module.ec2.public_ip
-}
-
-output "id" {
-  value = module.ec2.id
-}
