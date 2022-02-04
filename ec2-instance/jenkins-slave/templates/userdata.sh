cbd84ad8adc2d594802c3a31a323016881e1a8c7
hyun6ik <fkanwm@naver.com>
2022-02-04 15:21:41 +0900

    Fix #16 : Jenkins - Slave Userdata Java 11로 변경

--- ec2-instance/jenkins-slave/templates/userdata.sh
+++ ec2-instance/jenkins-slave/templates/userdata.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 sudo yum update
-sudo yum install docker git java-1.8.0-openjdk java-1.8.0-openjdk-devel.x86_64 -y
+sudo yum install docker git java-11-amazon-corretto.x86_64 -y
+
 
 sudo usermod -aG docker ec2-user
 
